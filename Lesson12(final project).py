c4cea10e451a4e92bdc9298d0a79c026fdcdba86
Shaff-I <iiiiiiiiiihor@gmail.com>
2025-05-11 12:11:45 +0300

    final project

--- Lesson12(final project).py
+++ Lesson12(final project).py
@@ -19,28 +19,25 @@ class Gadget_Aisle(Store):
             self.price = 550.0
         elif self.gadget == "TV":
             self.price = 1000.0
-    def buy(self):
-        print(f"Bought item {self.gadget} for {self.price}$")
 class Headphone_block(Gadget_Aisle):
     def __init__(self, gadget):
         super().__init__(gadget)
-        self.gadget = gadget
+        self.thing = gadget
         self.price = 0.0
         if self.gadget == "wireless headphones":
             self.price = 150.0
         elif self.gadget == "headphones with wires":
             self.price = 50.0
-    def buy(self):
-        print(f"Bought item {self.gadget} for {self.price}$")
+
+class Cash_register(Headphone_block):
+    def __init__(self, gadget):
+        super().__init__(gadget)
+    def buy(self, gadget):
+        print(f"Bought item {gadget.gadget} for {gadget.price}$")
+
 rozetka = Store()
 gadgetisle1 = Gadget_Aisle("computer")
 headphones1 = Headphone_block("wireless headphones")
-gadgetisle1.buy()
-headphones1.buy()
-
-'''
-class Cash_register(Store, Gadget_Aisle, Headphone_block):
-    def __init__(self):
-        super().__init__()
-        self.price =
-'''
+cashregister1 = Cash_register(gadgetisle1)
+cashregister1.buy(gadgetisle1)
+cashregister1.buy(headphones1)
